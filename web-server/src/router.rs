74998d05dba310515be5730f3ad6dfa79a3d0ca4
tanghy <tanghy@cloudthink.space>
2026-02-05 17:33:59 +0800

    完善十里

--- web-server/src/router.rs
+++ web-server/src/router.rs
@@ -1,4 +1,15 @@
 use axum::{Router, extract::Path, http::StatusCode, response::IntoResponse, routing::get};
+use chrono::NaiveDateTime;
+use serde::{Deserialize, Serialize};
+use server_middleware::extract::db::DbPool;
+use sqlx::prelude::FromRow;
+
+#[derive(Debug, Serialize, Deserialize, FromRow)]
+struct RequestLog {
+    id: String,
+    path: String,
+    created_at: NaiveDateTime,
+}
 
 pub fn app_router() -> Router {
     Router::new()
@@ -15,6 +26,12 @@ pub async fn root_id(Path(id): Path<String>) -> impl IntoResponse {
     (StatusCode::OK, format!("hello world: {}", id))
 }
 
-pub async fn aaa() -> impl IntoResponse {
-    (StatusCode::OK, format!("hello world: {}", "aaa"))
+pub async fn aaa(DbPool(pool): DbPool) -> impl IntoResponse {
+    let res = sqlx::query_as!(RequestLog, "SELECT * FROM request_logs where id = $1", "1")
+        .fetch_one(&*pool)
+        .await;
+    match res {
+        Ok(data) => (StatusCode::OK, format!("hello world: {:?}", data)),
+        Err(_) => (StatusCode::OK, format!("hello world: {}", "error")),
+    }
 }
